a88cdf68b9c66084ae7ee0d925dfd83dbb0f4bcf
Basil Meer <basilmeer@gmail.com>
2024-09-09 13:27:41 +0500

    devise setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_09_081916) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_09_082312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_09_081916) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "gender", default: 0, null: false
+    t.string "dob"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -50,9 +54,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_09_081916) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "jti"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["jti"], name: "index_users_on_jti"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
