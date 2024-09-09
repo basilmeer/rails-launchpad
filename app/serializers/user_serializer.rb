491b8c26aaeb1771fb8c84eb4fc935735626bdee
Basil Meer <basilmeer@gmail.com>
2024-09-09 18:54:14 +0500

    added annotations

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  dob                    :string
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  first_name             :string
+#  gender                 :integer          default("male"), not null
+#  jti                    :string           not null
+#  last_name              :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_jti                   (jti) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
 class UserSerializer < BaseSerializer
   attributes :id, :first_name, :last_name, :email, :gender, :dob
 end
